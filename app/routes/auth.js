2f84b159f04e25ecc3d704fc6f652beaa46a13d7
hoanghuy-huy <huynhhoanghuy221122@gmail.com>
2023-11-17 12:20:46 +0700

    back-up

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,8 +4,7 @@ const middlewareController = require('../controllers/MiddlewareController')
 const authController = require('../controllers/AuthController')
 
 
-// router.post('/reset-password',authController.resetPassword)
-// router.post('/forgot-password',authController.forgotPassword)
+
 router.post('/logout',middlewareController.verifyToken,authController.userLogout)
 router.post('/refresh',authController.refresh)
 router.post('/login',authController.login)
